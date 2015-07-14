ddd10a0c3446339dfcc83f3c6134f755cc45fca1
nemyup@163.com <nemyup@163.com>
2015-07-14 17:35:45 +0800

    add notes

--- CRedisPool.cpp
+++ CRedisPool.cpp
@@ -1,34 +1,46 @@
 /**
- * Copyright (c) 2015, 爱wifi（版权声明）
+ * Copyright (c) 2015, iwifi (Copyright statement)
  *
  * @file	CRedisPool.cpp		
- * @brief 此文件的简单描述。(必填字段)
- *
- * 此文件的详细功能描述。(可选字段)
- *
+ * @brief CRedisPool class is to create and manage redis connections in the buffer pool.
+ * These connections are ready to be used by any thread that needs them.
  * @author: 		yp
  * @date: 		Jul 6, 2015
  *
- * 修订说明:初始版本
+ * Revision Description: initial version
  */
 #include "CRedisPool.h"
 
 
 
 
+void OnRunCallBack(void* pVoid)
+{
+	CRedisPool* pRedisPool = (CRedisPool*)pVoid;
+	if(pRedisPool)
+	{
+		//The state of the connection pool is not close.
+		while(pRedisPool->status != CRedisPool::REDIS_POOL_DEAD)
+		{
+			::sleep(pRedisPool->scanTime);
+			pRedisPool->keepAlive();	//keep connecting
+		}
+	}
+}
+
+
 CRedisPool::CRedisPool()
 {
+	status = REDIS_POOL_UNCONN;
+	scanTime = 60;
+
 	_host.clear();
 	_port = 0;
 	_password.clear();
 	_timeout = 0;
-
 	_minSize = 5;
 	_maxSize = 10;
-
 	_connList.clear();
-	_bExitRun = false;
-	_scanTime = 60;
 	_idleTime = 60;
 }
 
@@ -41,15 +53,17 @@ CRedisPool::~CRedisPool()
 
 
 bool CRedisPool::init(const std::string& host, uint16_t port, const std::string& password, uint32_t timeout,
-		uint16_t minSize, uint16_t  maxSize, uint32_t scanTime, uint32_t idleTime)
+		uint16_t minSize, uint16_t  maxSize, uint32_t nScanTime, uint32_t idleTime)
 {
+	scanTime = nScanTime;
+
 	_host = host;
 	_port = port;
 	_password = password;
 	_timeout = timeout;
-	_scanTime = scanTime;
 	_idleTime = idleTime;
 
+	//the minSize value is less than the maxSize value
 	if (minSize <= maxSize)
 	{
 		_maxSize = maxSize;
@@ -61,46 +75,53 @@ bool CRedisPool::init(const std::string& host, uint16_t port, const std::string&
 		return false;
 	}
 
+	_scanThread.start(OnRunCallBack, this);	//Starting scan thread
+
 	return true;
 }
 
 
 CRedisClient* CRedisPool::getConn(void)
 {
-	Poco::Mutex::ScopedLock lock(_mutex);
-	int idleCount = __getIdleCount();
+	if(status == REDIS_POOL_DEAD)	//If you closed the connection pool, return NULL
+		return NULL;
 
-	if(!idleCount)
+	Poco::Mutex::ScopedLock lock(_mutex);
+	int idleCount = __getIdleCount();	//get the count of idle connections in the pool
+
+	if(idleCount == 0)
 	{
-		if (_connList.size() >= _maxSize)
+		if ( _connList.size() >= _maxSize)
 		{
-			_cond.wait(_mutex);
+			_cond.wait(_mutex);	//There is no idle connection in the pool, waiting...
 		}
-		else
+		else	//The count of connected pool is not up to the maximum.
 		{
-			SRedisConn* redisConn = new SRedisConn;
+			SRedisConn* pRedisConn = new SRedisConn;
 
 			try {
-				redisConn->pConn.connect(_host, _port);
-			} catch (Poco::Exception& e) {
+				pRedisConn->conn.connect(_host, _port);
+			} catch (Poco::Exception& e) {	//The connection timeout, throw exception
 				DEBUGOUT("CRedisPool::getConn:------Error:---", e.message());
-				if(redisConn) {
-					delete redisConn;
-					redisConn = NULL;
+				if(pRedisConn) {
+					delete pRedisConn;
+					pRedisConn = NULL;
 				}
 				throw ConnectErr("This connection is timeout");
 			}
 
-			if(redisConn) {
-				redisConn->idle = false;
-				_connList.push_back(redisConn);
-				return &redisConn->pConn;
+			if(pRedisConn) {
+				pRedisConn->idle = false;
+				_connList.push_back(pRedisConn); //Create success, push into the connection pool
+				status = REDIS_POOL_WORKING;
+				return &pRedisConn->conn;
+			} else {
+				return NULL;
 			}
-
-			return NULL;
 		}
 	}
 
+	//traverse connection pool, return idle connection
 	RedisConnIter iter = _connList.begin();
 	for( ; iter != _connList.end(); iter++)
 	{
@@ -108,7 +129,7 @@ CRedisClient* CRedisPool::getConn(void)
 		if(redisConn && redisConn->idle)
 		{
 			redisConn->idle = false;
-			return &redisConn->pConn;
+			return &redisConn->conn;
 		}
 	}
 
@@ -121,40 +142,44 @@ void CRedisPool::pushBackConn(CRedisClient* & pConn)
 	Poco::Mutex::ScopedLock lock(_mutex);
 	RedisConnIter iter = _connList.begin();
 
+	int idleCount = __getIdleCount();
 	for( ; iter != _connList.end(); iter++)
 	{
-		SRedisConn* redisConn = *iter;
-		if (redisConn && (&redisConn->pConn == pConn))
+		SRedisConn* pRedisConn = *iter;
+		if (pRedisConn && (&pRedisConn->conn == pConn)) //find pConn
 		{
-			redisConn->idle = true;
-			redisConn->oldTime = time(0);
+			pRedisConn->idle = true;
+			pRedisConn->oldTime = time(0); //Record the time stamp
 			pConn = NULL;
+
+			if(status == REDIS_POOL_DEAD)
+				delete pRedisConn;//If you closed the connection pool, delete pConn
+			else {
+				if(idleCount == 0)
+					_cond.signal();//wake up waiting
+			}
 			break;
 		}
 	}
 
-	if(__getIdleCount() == 1)
-		_cond.signal();
 }
 
 
 void CRedisPool::closeConnPool(void)
 {
-	Poco::Mutex::ScopedLock lock(_mutex);
-	RedisConnIter iter = _connList.begin();
+	_mutex.lock();
+	status = REDIS_POOL_DEAD;
+	_cond.broadcast();	//Wake up all the wait
 
+	RedisConnIter iter = _connList.begin();
 	for( ; iter != _connList.end(); iter++)
 	{
-		SRedisConn* redisConn = *iter;
-		if (redisConn)
-		{
-			redisConn->pConn.closeConnect();
-			delete redisConn;
-		}
+		SRedisConn* pRedisConn = *iter;
+		if (pRedisConn && pRedisConn->idle)
+			delete pRedisConn;	//Free the idle connection
 	}
-
-	_connList.clear();
-	_bExitRun = true;
+	_mutex.unlock();
+	_scanThread.join();	//Waiting for the thread to end
 }
 
 
@@ -163,22 +188,23 @@ void CRedisPool::keepAlive(void)
 	Poco::Mutex::ScopedLock lock(_mutex);
 	RedisConnIter iter = _connList.begin();
 
-	int leftSize = _connList.size() - _minSize;
+	int leftSize = _connList.size() - _minSize; //Keep the minimum count of connections in the pool
 	for( ; iter != _connList.end(); )
 	{
-		SRedisConn* redisConn = *iter;
-		if (redisConn)
+		SRedisConn* pRedisConn = *iter;
+		if (pRedisConn)
 		{
-			if((leftSize > 0) && redisConn->idle && (time(0) - redisConn->oldTime) > _idleTime)
+			//The idle time for idle connections over the maximum of the idle time
+			if((leftSize > 0) && pRedisConn->idle && (time(0) - pRedisConn->oldTime) > _idleTime)
 			{
-				delete redisConn;
+				delete pRedisConn;
 				_connList.erase(iter);
 				leftSize --;
 			}
-			else
+			else // if disconnected will be reconnect
 			{
-				if(!redisConn->pConn.ping())
-					redisConn->pConn.reconnect();
+				if(!pRedisConn->conn.ping())
+					pRedisConn->conn.reconnect();
 				iter++;
 			}
 		}
@@ -189,16 +215,6 @@ void CRedisPool::keepAlive(void)
 }
 
 
-void CRedisPool::run()
-{
-	while(!_bExitRun)
-	{
-		::sleep(_scanTime);
-		keepAlive();
-	}
-}
-
-
 int CRedisPool::__getIdleCount(void)
 {
 	int idleCount = 0;
@@ -206,8 +222,8 @@ int CRedisPool::__getIdleCount(void)
 	RedisConnIter iter = _connList.begin();
 	for( ; iter != _connList.end(); iter++)
 	{
-		SRedisConn* redisConn = *iter;
-		if(redisConn && redisConn->idle)
+		SRedisConn* pRedisConn = *iter;
+		if(pRedisConn && pRedisConn->idle)
 		{
 			idleCount ++;
 		}
--- CRedisPool.h
+++ CRedisPool.h
@@ -1,15 +1,13 @@
 /**
- * Copyright (c) 2015, 爱wifi（版权声明）
+ * Copyright (c) 2015, iwifi (Copyright statement)
  *
  * @file	CRedisPool.h		
- * @brief 此文件的简单描述。(必填字段)
- *
- * 此文件的详细功能描述。(可选字段)
- *
+ * @brief CRedisPool class is to create and manage redis connections in the buffer pool.
+ * These connections are ready to be used by any thread that needs them.
  * @author: 		yp
  * @date: 		Jul 6, 2015
  *
- * 修订说明:初始版本
+ * Revision Description: initial version
  */
 #ifndef CREDISCLIENT_CREDISPOOL_H_
 #define CREDISCLIENT_CREDISPOOL_H_
@@ -21,57 +19,104 @@
 
 
 
-class CRedisPool : public Poco::Runnable
+class CRedisPool
 {
 public:
-
+	///< single connection
 	typedef struct
 	{
-		CRedisClient pConn;
-		time_t oldTime;
-		bool idle;
+		CRedisClient conn;	///< connection
+		time_t oldTime;		///< idle time stamp
+		bool idle;		///< idle state
 	} SRedisConn;
 
 
-	///< 连接链表
+	///< redis pool state
+	typedef enum
+	{
+	    REDIS_POOL_UNCONN = 0,	///< no connection
+	    REDIS_POOL_WORKING,		///< connecting
+	    REDIS_POOL_DEAD		///< disconnect
+	}REDIS_POOL_STATE;
+
+
+	///< linked list
 	typedef std::list<SRedisConn*> RedisConnList;
 	typedef std::list<SRedisConn*>::iterator RedisConnIter;
+
 
 	CRedisPool();
 	~CRedisPool();
 
 
+	/**
+	* @brief initial connection pool, starting scan thread
+	* @param host [in] host ip
+	* @param port [in] host port
+	* @param password [in] host password
+	* @param timeout [in] timeout period, default 0
+	* @param minSize [in] minimum value of connections, default 5
+	* @param maxSize [in] maximum value of connections, default 10
+	* @param nScanTime [in] thread scan time, default 60
+	* @param idleTime [in] idle time, default 60
+	* @return if success return true else return false
+	* @warning If the maximum value is less than the minimum value will initialize the failure.
+	*/
 	bool init(const std::string& host, uint16_t port, const std::string& password, uint32_t timeout=0,
-			uint16_t minSize=5, uint16_t  maxSize=10, uint32_t scanTime = 60, uint32_t idleTime = 60);
+			uint16_t minSize=5, uint16_t  maxSize=10, uint32_t nScanTime = 60, uint32_t idleTime = 60);
 
+	/**
+	* @brief get a single connection in the pool
+	* @return return a connection, if busy will wait
+	* @warning The connection timeout will throw exception.
+	*/
 	CRedisClient* getConn(void);
 
+	/**
+	* @brief put back a connection to the pool
+	* @param pConn [in and out] a connection reference
+	* @warning pConn will be set free.If you close the connection pool, not pushBackConn will cause the memory leak.
+	*/
 	void pushBackConn(CRedisClient* & pConn);
 
+	/**
+	* @brief traverse connection pool, if disconnected will be reconnect
+	* @warning If the idle time is reached, the connection will be released.
+	*/
 	void keepAlive(void);
 
+	/**
+	* @brief close connection pool
+	* @warning Free idle connection, waiting for the scan thread to end.
+	*/
 	void closeConnPool(void);
 
-	virtual void run();
 
+	uint32_t scanTime;		///< thread scan time, unit: Second
+	REDIS_POOL_STATE status;	///< redis pool state
+	Poco::Thread _scanThread;	///< scan thread, if disconnected will be reconnect
 private:
-	std::string _host;
-	uint16_t _port;
-	std::string _password;
-	uint32_t _timeout;
-	uint16_t _minSize;
-	uint16_t _maxSize;
+	std::string _host;		///< host ip
+	uint16_t _port;			///< host port
+	std::string _password;		///< host password
+	uint32_t _timeout;		///< timeout period, default 0
+	uint16_t _minSize;		///< minimum value of connections, default 5
+	uint16_t _maxSize;		///< maximum value of connections, default 10
 
-	RedisConnList _connList;
+	RedisConnList _connList;	///< the list of redis connection pool
+	//Poco::Thread _scanThread;	///< scan thread, if disconnected will be reconnect
+	uint32_t _idleTime;		///< connection idle time, unit: Second
 
-	bool _bExitRun;
-	uint32_t _scanTime;	///< 线程扫描周期 单位:秒
-	uint32_t _idleTime;	///< 连接空闲时间 单位:秒
 	Poco::Mutex _mutex;
 	Poco::Condition _cond;
-
 	DISALLOW_COPY_AND_ASSIGN(CRedisPool);
+
+	/**
+	* @brief get the count of idle connections in the pool
+	* @return return the count of idle connections
+	*/
 	int __getIdleCount(void);
+
 
 };
 
