07289965538324ff393e2be51c851cab763da572
yuhaiyang <18989893637@189.cn>
2016-05-16 11:39:10 +0800

    增加了库的项目文件 .pro

---
+++ redis-client/Redisclient.cpp
@@ -0,0 +1,18 @@
+/**
+* Copyright (c) 2015, 爱wifi（版权声明）
+*
+* @file xxxxxxx
+* @brief 文件的简单介绍
+*
+* 文件的详细介绍
+*
+* @author: yuhaiyang
+* @date: 2016/5/16
+*/
+
+#include "Redisclient.h"
+
+
+Redisclient::Redisclient()
+{
+}
